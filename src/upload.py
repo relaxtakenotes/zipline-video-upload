ac3b6cf6158198d8109e30ea9c278771ea03c12a
relaxtakenotes <91839650+relaxtakenotes@users.noreply.github.com>
2024-04-22 18:57:12 +0500

    add losslesscut automation

--- src/upload.py
+++ src/upload.py
@@ -9,10 +9,11 @@ import string
 import subprocess
 import shlex
 import traceback
+import time
 
-TOKEN = "alotofletters"
+TOKEN = ""
 CHUNK_SIZE = 20_000_000
-DOMAIN = "https://example.com"
+DOMAIN = "https://cool.com"
 
 def upload(file_path):
     chunks = []
@@ -32,6 +33,8 @@ def upload(file_path):
     last_chunk_pos = 0
     for i, chunk in enumerate(chunks):
         chunk_length = len(chunk)
+
+        print(f"Uploading chunk {i + 1}/{chunks_length}")
 
         response = httpx.post(DOMAIN + "/api/upload", 
             timeout = 9999,
@@ -71,25 +74,59 @@ def main():
     if not input().lower().startswith("y"):
         exit()
 
-    i_base, _ = os.path.splitext(file_path)
+    pipe = subprocess.PIPE
+
+    print("Opening LosslessCut...")
+
+    ignore = {}
+
+    dirname = os.path.dirname(file_path)
+    files = os.listdir(dirname)
+
+    for file in files:
+        input_file_base, input_file_ext = os.path.splitext(os.path.basename(file_path))
+        current_file_base, current_file_ext = os.path.splitext(os.path.basename(file))
+        if input_file_ext == current_file_ext and input_file_base in current_file_base and input_file_base != current_file_base:
+            ignore[file] = True
+            
+    subprocess.Popen(shlex.split(f"LosslessCut \"{file_path}\""), stdout=pipe).communicate()
+    time.sleep(1)
+    
+    done = False
+    cut_path = ""
+    while not done:
+        files = os.listdir(dirname)
+
+        for file in files:
+            input_file_base, input_file_ext = os.path.splitext(os.path.basename(file_path))
+            current_file_base, current_file_ext = os.path.splitext(os.path.basename(file))
+            if input_file_ext == current_file_ext and input_file_base in current_file_base and input_file_base != current_file_base and not ignore.get(file):
+                done = True
+                cut_path = os.path.join(dirname, file)
+                break
+        
+        time.sleep(1)
+
+    i_base, _ = os.path.splitext(cut_path)
     intermediate_path = i_base + "_merged.mp4"
 
-    print("Merging audio tracks...")
-    num_of_inputs, _ = subprocess.Popen(shlex.split(f"ffprobe -loglevel error -select_streams a -show_entries stream=index -of csv=p=0 \"{file_path}\""), stdout=subprocess.PIPE).communicate()
+    print(f"Merging audio tracks... [{cut_path} --> {intermediate_path}]")
+    num_of_inputs, _ = subprocess.Popen(shlex.split(f"ffprobe -loglevel error -select_streams a -show_entries stream=index -of csv=p=0 \"{cut_path}\""), stdout=pipe).communicate()
     num_of_inputs = num_of_inputs.count(b"\n")
-    subprocess.Popen(shlex.split(f"ffmpeg -i \"{file_path}\" -c:v copy -c:a aac -b:a 160k -ac 2 -filter_complex \"amerge=inputs={num_of_inputs}\" \"{intermediate_path}\""), stdout=subprocess.PIPE).communicate()
+    subprocess.Popen(shlex.split(f"ffmpeg -i \"{cut_path}\" -c:v copy -c:a aac -b:a 160k -ac 2 -filter_complex \"amerge=inputs={num_of_inputs}\" \"{intermediate_path}\""), stdout=pipe).communicate()
     
     base, _ = os.path.splitext(intermediate_path)
     end_path = base + "_braked.mp4"
 
-    print(f"Converting...")
-    subprocess.Popen(shlex.split(f"HandBrakeCLI --preset-import-file \"{os.path.abspath("preset.json")}\" -Z \"Source 1080P\" -i \"{intermediate_path}\" -o \"{end_path}\""), stdout=subprocess.PIPE).communicate()
+    print(f"Converting... [{intermediate_path} --> {end_path}]")
+    subprocess.Popen(shlex.split(f"HandBrakeCLI --preset-import-file \"{os.path.abspath("preset.json")}\" -Z \"Source 1080P\" -i \"{intermediate_path}\" -o \"{end_path}\""), stdout=pipe).communicate()
 
-    print(f"Uploading...")
+    print(f"Uploading... [{end_path}]")
     upload(end_path)
 
     os.remove(end_path)
     os.remove(intermediate_path)
+    os.remove(cut_path)
 
 try:
     main()
